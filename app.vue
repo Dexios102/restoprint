7f4e2bf972480fa2e39909d55ca3614e5e083fb3
sunsigu <167049010+aguirremac17@users.noreply.github.com>
2025-03-18 15:06:15 +0800

    chore: log actions

--- app.vue
+++ app.vue
@@ -2,72 +2,108 @@
   <div>
     <div id="printable-content">
       <h2>Invoice</h2>
-      <p>Date: {{ new Date().toLocaleDateString() }}</p>
-      <p>Item: Sample Product</p>
-      <p>Price: $100</p>
+      <p>Star Clothing Boutique\n123 Star Road\nCity, State 12345</p>
     </div>
 
-    <button @click="printContent">🖨 Print</button>
-    <pre>{{ ports }}</pre>
+    <button @click="showPorts">🖨 {{ loadingPorts ? 'Showing..' : 'Show Available Devices' }}</button>
+    <template v-if="ports.length > 0">
+      <h5>Available Devices:</h5>
+      <template>
+        <li v-for="port in ports" :key="port.portName">
+          <p>{{ port.modelName }}</p>
+          <button @click="connectToPrinter(port)">{{ port == selectedDevice ? 'Connected' : 'Connect' }}</button>
+        </li>
+      </template>
+    </template>
+    <h5 v-else-if="ports.length == 0 && !loadingPorts && showPortsTriggered">No connected devices available.</h5>
+
+
+    <template v-if="selectedDevice">
+      <h4>Print Now</h4>
+      <button @click="handlePrint">Print</button>
+    </template>
+
+
+    <template v-if="connectionResult">
+      <h5>Connection Result:</h5>
+      <pre>{{ connectionResult }}</pre>
+    </template>
+
+    <template v-if="printResult">
+      <h5>Print Result</h5>
+      <pre>{{ printResult }}</pre>
+    </template>
   </div>
-  <CardModal v-model="showPortsModal" :ports="ports"  />
 </template>
 
 <script setup lang="ts">
 // reference https://github.com/auctifera-josed/starprnt
 import { StarPRNT } from '@awesome-cordova-plugins/star-prnt';
 
-const ports = ref<any>([])
-const showPortsModal = ref(false)
+const ports = ref<Port[]>([])
+const showPortsTriggered = ref(false)
+const loadingPorts = ref(false)
+const selectedDevice = ref<Port | null>(null)
+let hasBarcodeReader = true;
+let emulation = 'StarPRNT';
+
+const connectionResult = ref()
+const printResult = ref()
 
 
-const printContent = async () => {
-  showPortsModal.value = true;
+
+const showPorts = async () => {
   try {
-
-    let hasBarcodeReader = true;
-
+    showPortsTriggered.value = true;
+    loadingPorts.value = true;
     const starprntObj = await StarPRNT;
     console.log('StarPRNT:', starprntObj);
 
-    let emulation = starprntObj.Emulation.StarPRNT;
-    
-
     // Discover printers available on all ports (Bluetooth, USB, Network)
-    ports.value = await StarPRNT.portDiscovery('All');
+    ports.value = await StarPRNT.portDiscovery('All') as Port[];
     console.log('Port Discovery:', ports);
 
     // Check if any printers were found
     if (ports.value.length === 0) {
       alert('No printers found!');
-      return;
-    } else {
-      showPortsModal.value = true;
     }
-
-    // Automatically choose the first available printer (you can modify this if needed)
-    const selectedPrinter = ports.value[0] as any;
-    console.log('Connecting to printer:', selectedPrinter);
-
-    // Connect to the selected printer
-    const connection = await StarPRNT.connect(selectedPrinter?.portName, emulation, hasBarcodeReader);
-    console.log('connection result', connection);
-
-    // Define the print object
-    const printObj = {
-      text: "Star Clothing Boutique\n123 Star Road\nCity, State 12345\n\n",
-      cutReceipt: true, // This tells the printer to cut the receipt after printing
-      openCashDrawer: true // This opens the cash drawer (if supported)
-    };
-
-    // Send the print job to the connected printer
-    await StarPRNT.printRawText(selectedPrinter?.portName, emulation, printObj)
-    console.log('Printing done!');
 
   } catch (error) {
     console.error('Error connecting to printers or printing:', error);
+  } finally {
+    loadingPorts.value = false;
   }
 };
+
+
+const connectToPrinter = async (port: Port) => {
+  selectedDevice.value = port
+
+  try {
+    // Connect to the selected printer
+    const connection = await StarPRNT.connect(selectedDevice.value?.portName, emulation, hasBarcodeReader);
+    connectionResult.value = connection as any;
+  } catch (error) {
+    alert("Connection Error: " + error);
+  }
+}
+
+const handlePrint = async () => {
+  // Define the print object
+  const printObj = {
+    text: "Star Clothing Boutique\n123 Star Road\nCity, State 12345\n\n",
+    cutReceipt: true, // This tells the printer to cut the receipt after printing
+    openCashDrawer: true // This opens the cash drawer (if supported)
+  };
+
+  try {
+    // Send the print job to the connected printer
+    printResult.value = await StarPRNT.printRawText(selectedDevice.value!.portName, emulation, printObj)
+  } catch (error) {
+    alert("Print Error: " + error);
+  }
+
+}
 
 
 </script>
