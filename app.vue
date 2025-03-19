cf47dfc62173e08adda86b08d1f9b83ccc87a471
sunsigu <167049010+aguirremac17@users.noreply.github.com>
2025-03-19 08:42:32 +0800

    chore: improve error handling

--- app.vue
+++ app.vue
@@ -1,58 +1,57 @@
 <template>
-    <div>
-        <div id="printable-content">
-            <h2>Invoice</h2>
-            <p>Star Clothing Boutique\n123 Star Road\nCity, State 12345</p>
-        </div>
+  <div>
+    <div id="printable-content">
+      <h2>Invoice</h2>
+      <p>Star Clothing Boutique\n123 Star Road\nCity, State 12345</p>
+    </div>
 
-        <button @click="showPorts">
-            🖨 {{ loadingPorts ? "Showing.." : "Show Available Devices" }}
-        </button>
-        <template v-if="ports.length > 0">
-            <h5>Available Devices:</h5>
-            <div
-                v-for="port in ports"
-                :key="port.portName"
-                style="
+    <button @click="showPorts">
+      🖨 {{ loadingPorts ? "Showing.." : "Show Available Devices" }}
+    </button>
+    <template v-if="ports.length > 0">
+      <h5>Available Devices:</h5>
+      <div v-for="port in ports" :key="port.portName" style="
                     display: flex;
                     justify-content: space-between;
                     align-items: center;
-                "
-            >
-                <span>
-                    <p>{{ port.modelName }}</p>
-                </span>
-                <button @click="connectToPrinter(port)">
-                    {{ port == selectedDevice ? "Connected" : "Connect" }}
-                </button>
-            </div>
-        </template>
-        <h5
-            v-else-if="ports.length == 0 && !loadingPorts && showPortsTriggered"
-        >
-            No connected devices available.
-        </h5>
+                ">
+        <span>
+          <p>{{ port.modelName }}</p>
+        </span>
+        <button @click="connectToPrinter(port)">
+          {{ port == selectedDevice ? "Selected" : "Select" }}
+        </button>
+      </div>
+    </template>
+    <h5 v-else-if="ports.length == 0 && !loadingPorts && showPortsTriggered">
+      No connected devices available.
+    </h5>
 
-        <template v-if="selectedDevice">
-            <h4>Print Now</h4>
-            <button @click="handlePrint">Print</button>
-        </template>
+    <template v-if="selectedDevice">
+      <h4>Print Now</h4>
+      <button @click="handlePrint">Print</button>
+    </template>
 
-        <template v-if="connectionResult">
-            <h5>Connection Result:</h5>
-            <pre>{{ connectionResult }}</pre>
-        </template>
+    <template v-if="checkPrinterStatusResult">
+      <h5>Check Printer Status Result:</h5>
+      <pre>{{ checkPrinterStatusResult }}</pre>
+    </template>
 
-        <template v-if="statusResult">
-            <h5>Printer Status Result</h5>
-            <pre>{{ statusResult }}</pre>
-        </template>
+    <template v-if="connectionResult">
+      <h5>Connection Result:</h5>
+      <pre>{{ connectionResult }}</pre>
+    </template>
 
-        <template v-if="printResult">
-            <h5>Print Result</h5>
-            <pre>{{ printResult }}</pre>
-        </template>
-    </div>
+    <template v-if="statusResult">
+      <h5>Printer Get Status Result</h5>
+      <pre>{{ statusResult }}</pre>
+    </template>
+
+    <template v-if="printResult">
+      <h5>Print Result</h5>
+      <pre>{{ printResult }}</pre>
+    </template>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -68,64 +67,80 @@ let emulation = StarPRNT.Emulation.StarLine;
 
 const connectionResult = ref();
 const printResult = ref();
+const checkPrinterStatusResult = ref();
 const statusResult = ref();
 
 const showPorts = async () => {
-    try {
-        showPortsTriggered.value = true;
-        loadingPorts.value = true;
-        const starprntObj = StarPRNT;
-        console.log("StarPRNT:", starprntObj);
+  try {
+    showPortsTriggered.value = true;
+    loadingPorts.value = true;
+    const starprntObj = StarPRNT;
+    console.log("StarPRNT:", starprntObj);
 
-        // Discover printers available on all ports (Bluetooth, USB, Network)
-        ports.value = (await StarPRNT.portDiscovery("All")) as Port[];
-        console.log("Port Discovery:", ports);
+    // Discover printers available on all ports (Bluetooth, USB, Network)
+    ports.value = (await StarPRNT.portDiscovery("All")) as Port[];
+    console.log("Port Discovery:", ports);
 
-        // Check if any printers were found
-        if (ports.value.length === 0) {
-            alert("No printers found!");
-        }
-    } catch (error) {
-        console.error("Error connecting to printers or printing:", error);
-    } finally {
-        loadingPorts.value = false;
+    // Check if any printers were found
+    if (ports.value.length === 0) {
+      alert("No printers found!");
     }
+  } catch (error) {
+    console.error("Error StarPRNT.portDiscovery:", error);
+  } finally {
+    loadingPorts.value = false;
+  }
 };
 
 const connectToPrinter = async (port: Port) => {
-    selectedDevice.value = port;
+  selectedDevice.value = port;
 
-    try {
-        // Connect to the selected printer
-        const connection = StarPRNT.connect(
-            selectedDevice.value?.portName,
-            emulation,
-            hasBarcodeReader
-        );
-        connectionResult.value = connection as any;
-    } catch (error) {
-        alert("Connection Error: " + error);
-    }
+  try {
+   checkPrinterStatusResult.value = await StarPRNT.checkStatus(selectedDevice.value!.portName, emulation);
+  } catch (error) {
+    console.log("Error StarPRNT.checkStatus:", error);
+  }
 };
 
 const handlePrint = async () => {
-    // Define the print object
-    const printObj = {
-        text: "Star Clothing Boutique\n123 Star Road\nCity, State 12345\n\n",
-        cutReceipt: true, // This tells the printer to cut the receipt after printing
-        openCashDrawer: false, // This opens the cash drawer (if supported)
-    };
+  printResult.value = null;
+  statusResult.value = null;
+  connectionResult.value = null;
 
-    try {
-        statusResult.value = StarPRNT.getStatus();
-        // Send the print job to the connected printer
-        printResult.value = await StarPRNT.printRawText(
-            selectedDevice.value!.portName,
-            emulation,
-            printObj
-        );
-    } catch (error) {
-        alert("Print Error: " + error);
-    }
+  if(!selectedDevice.value?.portName) {
+    alert("Printer Selected Error: No PortName")
+    return;
+  }
+
+  // Define the print object
+  const printObj = {
+    text: "Star Clothing Boutique\n123 Star Road\nCity, State 12345\n\n",
+    cutReceipt: true, // This tells the printer to cut the receipt after printing
+    openCashDrawer: false, // This opens the cash drawer (if supported)
+  };
+
+  try {
+    // Connect to the selected printer
+    const connection = StarPRNT.connect(
+      selectedDevice.value!.portName,
+      emulation,
+      hasBarcodeReader
+    );
+    connectionResult.value = connection as any;
+  } catch (error) {
+    alert("Error StarPRNT.connect: " + error);
+  }
+
+  try {
+    statusResult.value = StarPRNT.getStatus();
+    // Send the print job to the connected printer
+    printResult.value = await StarPRNT.printRawText(
+      selectedDevice.value!.portName,
+      emulation,
+      printObj
+    );
+  } catch (error) {
+    alert("Error StarPRNT.printRawText: " + error);
+  }
 };
 </script>
